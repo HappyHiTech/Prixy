9a27e4d78251a7152fafeae12095b0cef389fed7
HappyHiTech <harveycytan@gmail.com>
2026-09-05 10:11:12 -0700

    bug: keyboard dissmiss

--- frontend/src/screens/VerificationScreen/VerificationScreen.tsx
+++ frontend/src/screens/VerificationScreen/VerificationScreen.tsx
@@ -1,4 +1,11 @@
-import { View, Text, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+
 import { router } from 'expo-router';
 import GoBackButton from '@/components/GoBackButton/GoBackButton';
 import OnBoardingButton from '@/components/OnBoardingButton/OnBoardingButton';
@@ -9,61 +16,65 @@ import { styles } from './VerificationScreen.styles';
 
 const VerificationScreen = () => {
   return (
-    <View style={styles.container}>
-      <View>
-        <GoBackButton onPress={() => router.push('/')} />
-        <View style={styles.header}>
-          <Text style={styles.headerText}>We sent you a Verification Code</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View>
+          <GoBackButton onPress={() => router.push('/')} />
+          <View style={styles.header}>
+            <Text style={styles.headerText}>
+              We sent you a Verification Code
+            </Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              keyboardType="number-pad"
+              maxLength={1}
+              selectionColor={COLORS.primaryText}
+            />
+            <TextInput
+              style={styles.input}
+              keyboardType="number-pad"
+              maxLength={1}
+              selectionColor={COLORS.primaryText}
+            />
+            <TextInput
+              style={styles.input}
+              keyboardType="number-pad"
+              maxLength={1}
+              selectionColor={COLORS.primaryText}
+            />
+            <TextInput
+              style={styles.input}
+              keyboardType="number-pad"
+              maxLength={1}
+              selectionColor={COLORS.primaryText}
+            />
+            <TextInput
+              style={styles.input}
+              keyboardType="number-pad"
+              maxLength={1}
+              selectionColor={COLORS.primaryText}
+            />
+            <TextInput
+              style={styles.input}
+              keyboardType="number-pad"
+              maxLength={1}
+              selectionColor={COLORS.primaryText}
+            />
+          </View>
+          <Text style={styles.subtitle}>
+            {'Enter the 6-digit code sent to your email'}
+          </Text>
         </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            keyboardType="number-pad"
-            maxLength={1}
-            selectionColor={COLORS.primaryText}
-          />
-          <TextInput
-            style={styles.input}
-            keyboardType="number-pad"
-            maxLength={1}
-            selectionColor={COLORS.primaryText}
-          />
-          <TextInput
-            style={styles.input}
-            keyboardType="number-pad"
-            maxLength={1}
-            selectionColor={COLORS.primaryText}
-          />
-          <TextInput
-            style={styles.input}
-            keyboardType="number-pad"
-            maxLength={1}
-            selectionColor={COLORS.primaryText}
-          />
-          <TextInput
-            style={styles.input}
-            keyboardType="number-pad"
-            maxLength={1}
-            selectionColor={COLORS.primaryText}
-          />
-          <TextInput
-            style={styles.input}
-            keyboardType="number-pad"
-            maxLength={1}
-            selectionColor={COLORS.primaryText}
-          />
-        </View>
-        <Text style={styles.subtitle}>
-          {'Enter the 6-digit code sent to your email'}
-        </Text>
-      </View>
 
-      <OnBoardingButton
-        onPress={() => router.push('/home')}
-        buttonText="Verify"
-        style={styles.primaryButton}
-      />
-    </View>
+        <OnBoardingButton
+          onPress={() => router.push('/home')}
+          buttonText="Verify"
+          style={styles.primaryButton}
+        />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
