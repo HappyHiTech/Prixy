09eca7b44e3147202540f8d2e6456bd0453261a0
HappyHiTech <harveycytan@gmail.com>
2026-09-11 10:39:06 -0700

    fix(prayers): relax UUID pattern validation to accept any RFC-shaped UUID

--- backend/functions/prayers/get/index.js
+++ backend/functions/prayers/get/index.js
@@ -1,7 +1,7 @@
 const { withClient } = require("./shared/db");
 
 const UUID_PATTERN =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
 
 exports.handler = async (event) => {
   const sub = event.requestContext?.authorizer?.claims?.sub;
